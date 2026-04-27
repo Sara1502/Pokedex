dffa08629b94a9d0f7f441d6dd2baefadd914215
Sara Moreira <moreirasaragds@gmail.com>
2026-04-27 11:50:09 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
         <ol class="pokemons"  id="pokemonList">
@@ -24,15 +20,14 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i]
-            console.log(convertPokemonToLi(pokemon))
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons) => {
+    const listItens = []
+
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItens.push(convertPokemonToLi(pokemon))
+    }
+    pokemonList.innerHTML += convertPokemonToLi(pokemon)
+
+})
 
